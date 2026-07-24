b13ea772e17c2d3e51aae262f331042602ae54d0
AwningMike <mike@awningpartners.com>
2026-07-24 11:16:29 -0400

    Add Test Connection diagnostic (reports 401/403/404/200) for Airtable token; sw v18

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // APMG Quote Calculator service worker
 // Bump CACHE version whenever you deploy a new build so clients refresh cleanly.
-const CACHE = 'apmg-quote-calc-v17';
+const CACHE = 'apmg-quote-calc-v18';
 const ASSETS = [
   './canopy_quote_calculator.html',
   './manifest.json',
