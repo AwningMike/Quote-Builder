0c58d05eb2e8793e2801d016163f152594a78c77
AwningMike <mike@awningpartners.com>
2026-07-28 14:10:33 -0400

    Save name defaults to Project - Ship To City (no ZIP); bigger cloud-search box · SW v36

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // APMG Quote Calculator service worker
 // Bump CACHE version whenever you deploy a new build so clients refresh cleanly.
-const CACHE = 'apmg-quote-calc-v35';
+const CACHE = 'apmg-quote-calc-v36';
 const ASSETS = [
   './canopy_quote_calculator.html',
   './drawing_builder.html',
