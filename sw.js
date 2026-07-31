058ee41621fd9f5668462e1b414f7be8ecbec7ce
AwningMike <mike@awningpartners.com>
2026-07-31 14:13:47 -0400

    v61: drawing upgrades from approvals + BOM split to internal-only

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // APMG Quote Calculator service worker
 // Bump CACHE version whenever you deploy a new build so clients refresh cleanly.
-const CACHE = 'apmg-quote-calc-v60';
+const CACHE = 'apmg-quote-calc-v61';
 const ASSETS = [
   './canopy_quote_calculator.html',
   './drawing_builder.html',
