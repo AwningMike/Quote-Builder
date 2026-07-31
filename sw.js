8e09aaa7c1f7cd3a3ceca51c2c2cbfed4336c5a3
AwningMike <mike@awningpartners.com>
2026-07-31 15:46:00 -0400

    v65: pans by W x P x 2 formula plus 5 pct drop

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // APMG Quote Calculator service worker
 // Bump CACHE version whenever you deploy a new build so clients refresh cleanly.
-const CACHE = 'apmg-quote-calc-v64';
+const CACHE = 'apmg-quote-calc-v65';
 const ASSETS = [
   './canopy_quote_calculator.html',
   './drawing_builder.html',
