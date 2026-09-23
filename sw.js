98a990c6fb13b13ac2c6f56bbc8c349a5dc15f83
AwningMike <mike@awningpartners.com>
2026-09-23 12:43:40 -0400

    v100 - fabric builder cleanup: dedupe recover survey/delivery, separate Install+Delivery lines, new Airtable log format

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // APMG Quote Calculator service worker
 // Bump CACHE version whenever you deploy a new build so clients refresh cleanly.
-const CACHE = 'apmg-quote-calc-v99';
+const CACHE = 'apmg-quote-calc-v100';
 const ASSETS = [
   './canopy_quote_calculator.html',
   './fabric_quote_calculator.html',
